522893bc0482f5ce0ec03173ccbef7992c09f3b4
darookee <info@darookee.net>
2012-03-01 16:21:25 +0100

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ NEWNAME=${1:-Boilerplate}
 
 echo "Creating Plugin ${NEWNAME}"
 
-find . ! -path "*./.*" -type f -exec sed -i 's/Boilerplate/${NEWNAME}/g' {} \;
-git mv templates/backend/boilerplate templates/backend/${NEWNAME,,} -fr
-git mv BoilerplateBackend.php ${NEWNAME}Backend.php
-git mv BoilerplateFrontend.php ${NEWNAME}Frontend.php
+find . ! -path "*./.*" -type f -exec sed -i "s/Boilerplate/${NEWNAME}/g" -i "s/boilerplate/${NEWNAME,,}" {} \;
+git mv templates/backend/boilerplate templates/backend/${NEWNAME,,} -f
+git mv BoilerplateBackend.php ${NEWNAME}Backend.php -f
+git mv BoilerplateFrontend.php ${NEWNAME}Frontend.php -f
+echo "" > README.md
+git rm setup.sh -f
