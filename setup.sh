0421206ef7d49a2770722e3c98f37a8313af71e1
darookee <info@darookee.net>
2012-04-04 22:11:32 +0200

    changed setup and added support for plugin type initialization

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 NEWNAME=${1:-Boilerplate}
+NEWTYPE=${2:-Frontend}
 
-echo "Creating Plugin ${NEWNAME}"
+echo "Creating Plugin ${NEWNAME} for ${NEWTYPE}"
 
 if hash git 2>&- /dev/null; then
     git checkout -b ${NEWNAME,,}
@@ -10,9 +11,11 @@ fi
 
 find . ! -path "*./.*" -type f -exec sed -i "s/Boilerplate/${NEWNAME}/g" {} \;
 find . ! -path "*./.*" -type f -exec sed -i "s/boilerplate/${NEWNAME,,}/g" {} \;
-git mv templates/backend/boilerplate templates/backend/${NEWNAME,,} -f
+sed -i "s/Backend_Boilerplate/${NEWTYPE,,}_${NEWNAME,,}/g" Bootstrap.php
+git mv templates/backend templates/${NEWTYPE,,} -f
+git mv templates/${NEWTYPe,,}/boilerplate templates/${NEWTYPE,,}/${NEWNAME,,} -f
 git mv BoilerplateBackend.php ${NEWNAME}Backend.php -f
 git mv BoilerplateFrontend.php ${NEWNAME}Frontend.php -f
-echo "" > README.md
+echo "Shopware ${NEWTYPE}-Plugin ${NEWNAME}" > README.md
 git rm setup.sh -f
 
