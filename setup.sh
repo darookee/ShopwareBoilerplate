4ec59dc5c85f05d7c3b0d1584f702f921935501e
Nils Uliczka <nils.uliczka@darookee.net>
2015-01-23 15:58:16 +0100

    added Views.connexo to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ echo "Creating ${NEWTYPE}-Plugin ${NEWNAME}"
 sed -i "s/Frontend_Boilerplate/${NEWTYPE}_Boilerplate/g" Bootstrap.php
 find . ! -path "*./.*" -type f -exec sed -i "s/Boilerplate/${NEWNAME}/g" {} \;
 find . ! -path "*./.*" -type f -exec sed -i "s/boilerplate/${NEWNAME,,}/g" {} \;
-mv Views/backend/boilerplate Views/backend/${NEWNAME,,} -f
 mv Views/frontend/plugins/boilerplate Views/frontend/plugins/${NEWNAME,,} -f
+mv Views.connexo/frontend/plugins/boilerplate Views.connexo/frontend/plugins/${NEWNAME,,} -f
 mv Controllers/BoilerplateFrontend.php Controllers/${NEWNAME}Frontend.php -f
 echo "Shopware ${NEWTYPE}-Plugin ${NEWNAME}" > README.md
 rm setup.sh -f
